f551fdcd7a9bb6a548b627871f4d89b8377f401e
Nic Thibodeaux <dnt2111@gmail.com>
2019-07-06 10:07:44 -0400

    WFOM_Data_Viewer upadted

---
+++ Code/getrecentdataset.m
@@ -0,0 +1,13 @@
+function mouseid = getrecentdataset(initDir)
+dirs = dir(initDir);
+mousedirs_tmp = {dirs(:).name};
+mousedates_tmp = {dirs(:).date};
+mousedirs = {};
+mousedates = {};
+for i = 1:numel(mousedirs_tmp)
+    mousedirs{end+1} = mousedirs_tmp{i};
+    mousedates{end+1} = mousedates_tmp{i};
+end
+
+mousedates = cellfun(@datenum,mousedates);
+mouseid = mousedirs{find(mousedates==max(mousedates))};
